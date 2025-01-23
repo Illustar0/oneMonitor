c3dd595be642131d122166e247b9a50e1209c981
Illustar0 <me@illustar0.com>
2025-01-23 20:23:25 +0800

    feat: initial support for put and delete methods feat: data synchronization between worker and server refactor: modify some for in statements
    Signed-off-by: Illustar0 <me@illustar0.com>

--- onemonitor/server.py
+++ onemonitor/server.py
@@ -90,7 +90,6 @@ async def init_room(
                 description TEXT
                 );"""
         )
-        conn.commit()
         cursor.execute(
             f"""REPLACE INTO rooms VALUES (
                            '{room.id}',
@@ -99,13 +98,62 @@ async def init_room(
                            '{room.description}'  
                            );"""
         )
-        conn.commit()
         cursor.execute(
             f"""CREATE TABLE IF NOT EXISTS {room.table_name}(
                     timestamp INT PRIMARY KEY NOT NULL,
                     electricity INT NOT NULL
                     );"""
         )
+        conn.commit()
+        status_code, status, msg, data = 200, "success", "", ""
+    except sqlite3.Error as e:
+        status_code, status, msg, data = 500, "error", str(e), ""
+    finally:
+        cursor.close()
+    return _restful_result(status_code, status, msg, data)
+
+
+@app.put("/rooms/{room}")
+async def update_room(
+    room: str,
+    room_updated: Room,
+    conn: sqlite3.Connection = Depends(get_db),
+    Authorization: str | None = Cookie(default=None),
+):
+    check_result = check_auth(Authorization)
+    if check_result is not None:
+        return check_result
+    cursor = conn.cursor()
+    try:
+        cursor.execute(
+            f"""UPDATE rooms SET name = '{room_updated.name}', table_name = '{room_updated.table_name}', description = '{room_updated.description}' WHERE id = '{room}';"""
+        )
+        conn.commit()
+        status_code, status, msg, data = 200, "success", "", ""
+    except sqlite3.Error as e:
+        status_code, status, msg, data = 500, "error", str(e), ""
+    finally:
+        cursor.close()
+    return _restful_result(status_code, status, msg, data)
+
+
+@app.delete("/rooms/{room}")
+async def delete_room(
+    room: str,
+    conn: sqlite3.Connection = Depends(get_db),
+    Authorization: str | None = Cookie(default=None),
+):
+    check_result = check_auth(Authorization)
+    if check_result is not None:
+        return check_result
+    cursor = conn.cursor()
+    if room == "rooms":
+        status_code, status, msg, data = 403, "error", "Forbidden", ""
+        return _restful_result(status_code, status, msg, data)
+    try:
+        room_table_name = "room_" + room.replace("-", "_")
+        cursor.execute(f"""DELETE FROM rooms WHERE id = '{room}';""")
+        cursor.execute(f"""DROP TABLE IF EXISTS {room_table_name};""")
         conn.commit()
         status_code, status, msg, data = 200, "success", "", ""
     except sqlite3.Error as e:
@@ -163,7 +211,7 @@ async def info(
     try:
         cursor.execute(f"""SELECT * FROM rooms""")
         rows = cursor.fetchall()
-        cursor.execute(f"PRAGMA table_info(rooms)")
+        cursor.execute(f"""PRAGMA table_info(rooms)""")
         columns = [row[1] for row in cursor.fetchall()]
         status_code, status, msg, data = (
             200,
--- onemonitor/worker.py
+++ onemonitor/worker.py
@@ -1,4 +1,5 @@
 import json
+import sys
 import time
 import toml
 import httpx
@@ -15,65 +16,126 @@ authkey = config["setting"]["authKey"]
 
 
 rooms = [data for i, data in config["room"].items()]
+room_id_list = [rooms[i]["id"] for i in range(len(rooms))]
+id2room_index = {
+    rooms[room_index]["id"]: room_index for room_index in range(len(rooms))
+}
 
-logger.info("Initialize the room database")
 
-# 初始化 room 的数据库
-for i in range(len(rooms)):
-    data = {
-        "id": rooms[i]["id"],
-        "name": rooms[i]["name"],
-        "table_name": "room_" + rooms[i]["id"].replace("-", "_"),
-        "description": rooms[i]["description"],
-    }
+# 与云端数据同步
+def sync_data_with_cloud():
+    logger.info("Synchronize with cloud data")
     try:
-        response = httpx.post(
-            f"{api_endpoint}/rooms", cookies={"Authorization": f"{authkey}"}, json=data
+        response = httpx.get(
+            f"{api_endpoint}/rooms", cookies={"Authorization": f"{authkey}"}
         )
         if response.status_code != 200:
-            logger.error(f"(id: {rooms[i]["id"]}) Initialization failed, API error")
-            continue
+            logger.error(f"Failed to obtain room data from the cloud, API error")
+            sys.exit()
         response_json = json.loads(response.text)
         if response_json["code"] != 200:
             logger.error(
-                f"{rooms[i]["name"]} Initialization failed, API returns: {response_json["msg"]}"
+                f"Failed to obtain room data from the cloud, API returns: {response_json["msg"]}"
             )
-            continue
-        logger.info(f"{rooms[i]["name"]} Initialization success")
+            sys.exit()
     except Exception as e:
-        logger.error(f"An error occurred in the POST request, details: {e}")
+        logger.error(f"An error occurred in the GET request, details: {e}")
+        sys.exit()
+    response_rooms_data = json.loads(response.text)["data"]["data"]
+    remote_room_id_list = [
+        response_rooms_data[i][0] for i in range(len(response_rooms_data))
+    ]
+    rooms_should_be_delete = list(set(remote_room_id_list) - set(room_id_list))
+    rooms_should_be_add = list(set(room_id_list) - set(remote_room_id_list))
+    if rooms_should_be_delete is not None:
+        for room_id in rooms_should_be_delete:
+            try:
+                response = httpx.delete(
+                    f"{api_endpoint}/rooms/{room_id}",
+                    cookies={"Authorization": f"{authkey}"},
+                )
+                if response.status_code != 200:
+                    logger.error(
+                        f"An attempt to delete the room data with id = {room_id} failed, API error"
+                    )
+                    continue
+                response_json = json.loads(response.text)
+                if response_json["code"] != 200:
+                    logger.error(
+                        f"An attempt to delete the room data with id = {room_id} failed, API returns: {response_json["msg"]}"
+                    )
+                    continue
+                logger.info(
+                    f"Successfully deleted the room related data with id = {room_id}"
+                )
+            except Exception as e:
+                logger.error(f"An error occurred in the DELETE request, details: {e}")
+                continue
+    if rooms_should_be_add is not None:
+        for room_id in rooms_should_be_add:
+            i = id2room_index.get(room_id)
+            data = {
+                "id": rooms[i]["id"],
+                "name": rooms[i]["name"],
+                "table_name": "room_" + rooms[i]["id"].replace("-", "_"),
+                "description": rooms[i]["description"],
+            }
+            try:
+                response = httpx.post(
+                    f"{api_endpoint}/rooms",
+                    cookies={"Authorization": f"{authkey}"},
+                    json=data,
+                )
+                if response.status_code != 200:
+                    logger.error(
+                        f"Failed to initialize room data with id = {room_id}, API error"
+                    )
+                    continue
+                response_json = json.loads(response.text)
+                if response_json["code"] != 200:
+                    logger.error(
+                        f"Failed to initialize room data with id = {room_id}, API returns: {response_json["msg"]}"
+                    )
+                    continue
+                logger.info(f"Successfully initialized room data with id = {room_id}")
+            except Exception as e:
+                logger.error(f"An error occurred in the POST request, details: {e}")
+                continue
+    logger.info("Synchronization with cloud data completed")
 
 
 # 更新电量
 def update_electricity(usercode, passwd):
     me = ZZUPy(usercode, passwd)
     me.login()
-    for i in range(len(rooms)):
+    for room_id in room_id_list:
         time.sleep(1)
-        electricity = me.eCard.get_remaining_power(rooms[i]["id"])
+        electricity = me.eCard.get_remaining_power(room_id)
         try:
             response = httpx.post(
-                f"{api_endpoint}/rooms/{rooms[i]["id"]}",
+                f"{api_endpoint}/rooms/{room_id}",
                 cookies={"Authorization": f"{authkey}"},
                 json={"timestamp": int(time.time()), "electricity": float(electricity)},
             )
             if response.status_code != 200:
                 logger.error(
-                    f"Failed to add the electricity record of {rooms[i]["name"]}, API error"
+                    f"Failed to add the electricity record with id = {room_id}, API error"
                 )
                 continue
             response_json = json.loads(response.text)
             if response_json["code"] != 200:
                 logger.error(
-                    f"Failed to add the electricity record of {rooms[i]["name"]}, API returns: {response_json["msg"]}"
+                    f"Failed to add the electricity record with id = {room_id}, API returns: {response_json["msg"]}"
                 )
                 continue
-            logger.info(f"Successfully added {rooms[i]["name"]} electricity record")
+            logger.info(f"Successfully added the power data of room id = {room_id}")
         except Exception as e:
             logger.error(f"An error occurred in the POST request, details: {e}")
 
 
-while True:
-    update_electricity(usercode, password)
-    logger.info("This cycle ends, waiting to enter the next cycle")
-    time.sleep(interval)
+if __name__ == "__main__":
+    sync_data_with_cloud()
+    while True:
+        update_electricity(usercode, password)
+        logger.info("This cycle ends, waiting to enter the next cycle")
+        time.sleep(interval)
